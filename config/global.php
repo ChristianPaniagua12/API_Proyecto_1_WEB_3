477ab58dcf6c7f73e78a260af7c4fc89f248bbc2
CHRISTIAN_PC_12 <crisp314528@gmail.com>
2025-11-06 22:34:08 -0600

    global.php

--- config/global.php
+++ config/global.php
@@ -7,13 +7,13 @@ if ($databaseUrl) {
     define("DB_PORT", $parsedUrl["port"]);
     define("DB_NAME", ltrim($parsedUrl["path"], "/"));
     define("DB_USERNAME", $parsedUrl["user"]);
-    define("DB_PASSWORD", $parsedUrl["pass"]);
+    define("DB_PASSWORD", $parsedUrl["pass"] ?: "lwFsaDtBHQFwBfRDmOuZStrXClcENMed");
 } else {
     define("DB_HOST", getenv("MYSQLHOST") ?: "mysql.railway.internal");
     define("DB_PORT", getenv("MYSQLPORT") ?: "3306");
     define("DB_NAME", getenv("MYSQLDATABASE") ?: "proyecto1web");
     define("DB_USERNAME", getenv("MYSQLUSER") ?: "root");
-    define("DB_PASSWORD", getenv("MYSQLPASSWORD") ?: getenv("MYSQL_ROOT_PASSWORD") ?: "");
+    define("DB_PASSWORD", getenv("MYSQLPASSWORD") ?: getenv("MYSQL_ROOT_PASSWORD") ?: "lwFsaDtBHQFwBfRDmOuZStrXClcENMed");
 }
 
 define("DB_ENCODE", "utf8mb4");
