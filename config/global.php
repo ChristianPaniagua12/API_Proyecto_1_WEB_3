00f3b75f7bc6cee0b2cce597ee9400ba022b6258
CHRISTIAN_PC_12 <crisp314528@gmail.com>
2025-11-06 18:48:30 -0600

    correccion en credenciales para la bd

--- config/global.php
+++ config/global.php
@@ -1,10 +1,9 @@
 <?php
-define("DB_HOST", "mysql.railway.internal"); 
-define("DB_PORT", "3306"); 
+define("DB_HOST", "hopper.proxy.rlwy.net"); 
+define("DB_PORT", "43589"); 
 define("DB_NAME", "railway"); 
 define("DB_USERNAME", "root"); 
 define("DB_PASSWORD", "lwFsADtBHQFwBfRDmOuZStrXClcENMed"); 
-
 define("DB_ENCODE", "utf8");
 
 define("PRO_NOMBRE", "ITProyecto");
