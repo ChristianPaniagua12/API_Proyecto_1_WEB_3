589d41b97e41beacc89be1c3f3813ffd6583bfe9
CHRISTIAN_PC_12 <crisp314528@gmail.com>
2025-11-09 17:46:03 -0600

    correcion de un CORS, para agregar el header de codigo

--- ajax/productoController.php
+++ ajax/productoController.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Codigo");
 header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
