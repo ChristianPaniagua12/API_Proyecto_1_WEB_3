720af8bf16c07cfcd394feec7d59c3920025d45a
CHRISTIAN_PC_12 <crisp314528@gmail.com>
2025-11-09 22:05:47 -0600

    Ajuste en el modelo
    ajuste para que funcione con el host del API

--- modelos/Cliente.php
+++ modelos/Cliente.php
@@ -1,20 +1,26 @@
 <?php 
+require __DIR__ . "/../config/Conexion.php";
 
-require "../config/Conexion.php";
-
-Class Cliente
+class Cliente
 {
-    public function __construct()
-    {
-
-    }
+    public function __construct() {}
 
     public function insertar($cedula, $nombre, $telefono, $correo)
     {
         try {
+           
+            $sql_check = "SELECT 1 FROM cliente WHERE Cedula = '$cedula' LIMIT 1";
+            $res_check = ejecutarConsulta($sql_check);
+
+            if ($res_check && $res_check->fetch(PDO::FETCH_NUM)) {
+                return 1062;
+            }
+
             $sql = "INSERT INTO cliente (Cedula, Nombre, Telefono, Correo)
                     VALUES ('$cedula', '$nombre', '$telefono', '$correo')";
-            return ejecutarConsulta($sql);
+            $ok = ejecutarConsulta($sql);
+
+            return $ok ? 1 : 0;
         } catch (Exception $e) {
             return $e->getCode();
         }
